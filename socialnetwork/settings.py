51c166fe808d138c6622a5906f348073d99e65bf
Chetram Bassit <chetrambassit121@gmail.com>
2022-04-28 15:59:43 -0400

    setting up for heroku deplyment

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 # import django_on_heroku
-# import django_heroku
+import django_heroku
 # from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
